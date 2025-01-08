fb4cb75a544c1e66494b09948d01a3a7dd2494b7
hyerin-kang <hkang0420@gmail.com>
2025-01-08 21:13:24 +0900

    6까지 완료

--- 05.js
+++ 05.js
@@ -5,6 +5,15 @@
  * - 파라미터로 받는 배열에서 name이 일치하는 첫 객체를 반환합니다. (find 사용)
  * - 없다면 null 반환.
  *
+ users = {name:string} 이 [] 안에 있따
+ [{name: string}, {name: string} ]
+ name : 문자열
+
+ const users = [{name: "혜린'}, {name: '재혁'}];
+
+const name = '재혁';
+function findUserByName(users, name) { };
+findByUserName([{name: "혜린'}, {name: '재혁'}], '재혁');
  * @param {{name:string}[]} users
  * @param {string} name
  * @returns {{name:string}|null}
@@ -18,6 +27,7 @@
  * @param {any[]} arr
  * @param {any} value
  * @returns {number}
+ * 
  */
 
 // TODO: findUserByName 함수를 작성하세요.
@@ -28,7 +38,31 @@ function findUserByName(users, name) {
 }
 
 // TODO: findIndexOf 함수를 작성하세요.
-function findIndexOf() {}
+/*
+Array 인스턴스의 findIndex() 메서드
+주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 인덱스를 반환합니다. 만족하는 요소가 없으면 -1을 반환합니다.
+arr.findIndex(callback(element, index, array) {조건에 맞는 요소를 찾으면 true 반환});
+*/
+function findIndexOf(arr, value) {
+  //arr의 배열 중에 value값과 일치하는것의 인덱스를 반환하라.
+  let findVal = arr.findIndex((e) => e == value);
+  if (!findVal) return -1;
+  return findVal;
+}
+/*
+* - findIndexOf를 작성하세요.
+* - 파라미터로 받는 배열에서 value와 일치하는 첫 인덱스를 반환합니다. (findIndex 사용)
+* - 값이 없으면 -1을 반환합니다.
+
+파라미터로 받는 배열
+const arr = [] // 아무 형태든 상관 없다.
+value = any //아무 형태든 상관없다.
+리턴값은 숫자.
+
+@param {any[]} arr
+@param {any} value
+@returns {number}
+*/
 
 // export를 수정하지 마세요.
 export { findUserByName, findIndexOf };
--- 06.js
+++ 06.js
@@ -17,10 +17,27 @@
  */
 
 // TODO: hasNegative 함수를 작성하세요.
-function hasNegative() {}
+function hasNegative(arr) {
+  // 1. arr라는 [] 배열을 받는다.
+  // 2. 배열중 음수가 있는지 확인한다(some으로)
+  // 2-1 some()메서드 : 배열 안의 요소들이 조건에 하나라도 맞는지 테스트
+  // 3. 반환한다.
+  return arr.some((e) => e < 0);
+}
 
 // TODO: allPositive 함수를 작성하세요.
-function allPositive() {}
+function allPositive(arr) {
+  return arr.every((e) => e > 0);
+  /*
+1. arr이라는 숫자를 가지는 배열을 받는다.
+2. 받아온 배열이 양수인지 확인한다. 양수면 true, 음수면 false
+2-1 every() 메서드 : 배열 안의 모든 요소들이 함수 테스트를 통과하는지 테스트 > 불리언값 반환
+3. boolean 형태를 반환한다.
+- 파라미터로 받는 배열이 모두 양수인지 확인합니다. (every 사용)
+@param {number[]} arr
+@returns {boolean}
+*/
+}
 
 // export를 수정하지 마세요.
 export { hasNegative, allPositive };
